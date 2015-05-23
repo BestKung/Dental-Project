fa6b826fc05067fd745b06ba11004c3d190650d3
BestKung <best.kittiporn@gmail.com>
2015-05-23 11:55:57 +0700

    first

---
+++ src/test/java/dental/DentalApplicationTests.java
@@ -0,0 +1,18 @@
+package dental;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DentalApplication.class)
+@WebAppConfiguration
+public class DentalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
