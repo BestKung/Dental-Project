31a2150995d25d1ce596e8d1ceb19ed4e85b4385
Natthaphon Phanit <kekhuay@e-mail.ph>
2015-05-23 14:46:08 +0700

    add new file

---
+++ src/main/java/th/co/geniustree/dental/domain/User.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.geniustree.dental.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author kekhuay
+ */
+@Entity
+public class User implements Serializable {
+
+    @Id
+    private String email;
+    private String name;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.email);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        return true;
+    }
+}
--- src/test/java/th/co/geniustree/dental/DentalApplicationIT.java
+++ src/test/java/th/co/geniustree/dental/DentalApplicationIT.java
@@ -10,10 +10,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DentalApplication.class)
 @WebAppConfiguration
-public class DentalApplicationTests {
+public class DentalApplicationIT {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
